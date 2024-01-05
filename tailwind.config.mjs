e659d996e0220009a366fcccc7ac1aed2f1b76fd
SeroTomas <serocontacto@gmail.com>
2024-01-05 00:08:20 -0300

    finish

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,10 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+			fontFamily: {
+				"c-lexend": ["Lexend Deca", "sans-serif"],
+				"c-bigShoulders": ["Big Shoulders Display", "sans-serif"]
+			},
 			colors: {
 				"c-orange": "hsl(31, 77%, 52%)",
 				"c-darkCyan": "hsl(184, 100%, 22%)",
@@ -10,7 +14,7 @@ export default {
 				"c-transpWhite": "hsla(0, 0%, 100%, 0.75)",
 				"lightGray": "hsl(0, 0%, 95%)",
 			},
-			
+
 		},
 	},
 	plugins: [],
