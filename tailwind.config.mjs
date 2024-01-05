1ba98875cc77463b9b9b8215054ac27c6dbaf3c9
SeroTomas <serocontacto@gmail.com>
2024-01-04 22:34:37 -0300

    tailwind config and fonts added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"c-orange": "hsl(31, 77%, 52%)",
+				"c-darkCyan": "hsl(184, 100%, 22%)",
+				"c-veryDarkCyan": "hsl(179, 100%, 13%)",
+				"c-transpWhite": "hsla(0, 0%, 100%, 0.75)",
+				"lightGray": "hsl(0, 0%, 95%)",
+			},
+			
+		},
 	},
 	plugins: [],
 }
